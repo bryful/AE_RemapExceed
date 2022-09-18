60fb48e2791c9e1a7c64d575cf42db673636593e
bryful <bryful@gmail.com>
2022-09-18 18:30:43 +0900

    update

--- Script/AE_RemapExceed.jsx
+++ Script/AE_RemapExceed.jsx
@@ -55,8 +55,57 @@ if ( typeof (FsJSON) !== "object"){//デバッグ時はコメントアウトす�
     //選ばれたラジオボタン
     var selectedIndex = -1;
     // 空セル
-    var isNoUseBD = false;
+    var emptyCell = 0;
+    // 0:ブロックディゾルブ 1: リマップ最大値 2:不透明度
+    // 前後のin点out点の処理
+    var inOutPoint = true;
 	//------------------------
+    var savePref = function()
+    {
+        var f = new File(Folder.userData.fullName + "/ae_remap.pref");
+        var obj = {};
+        obj.emptyCell = emptyCell;
+        obj.inOutPoint = inOutPoint;
+
+        if (f.open("w")==true)
+        {
+            try{
+                f.write(obj.toSource());
+            }catch(e){
+            }finally{
+                f.close();
+            }
+        }
+    }
+    var loadPref = function()
+    {
+        var f = new File(Folder.userData.fullName + "/ae_remap.pref");
+        if (f.open("r")==true)
+        {
+            try{
+                var s = f.read();
+                var obj = eval(s);
+                var n = obj.emptyCell;
+                 if ( typeof(n)=="number")
+                {
+                     emptyCell = n;
+                }else{
+                    emptyCell = 1;
+                }
+                var b = obj.inOutPoint;
+                 if ( typeof(n)=="boolean")
+                {
+                     inOutPoint = b;
+                }else{
+                    inOutPoint = true;
+                }
+            }catch(e){
+            }finally{
+                f.close();
+            }
+        }
+    }
+    loadPref();
 	//-------------------------------------------------------------------------
     //json utils
 	function toJSON(obj)
@@ -163,6 +212,23 @@ if ( typeof (FsJSON) !== "object"){//デバッグ時はコメントアウトす�
             alert("レイヤを選んで")
         }else{
             ret = lyrs;
+        }
+		return ret;
+	}
+    // ********************************************************************************
+    var getLayerOne = function(cmp)
+	{
+		var ret = null;
+		if ( (cmp ==null)||(cmp ==undefined)||( (cmp instanceof CompItem)==false)) {
+			var ac = getActiveComp();
+			if (ac == null) return ret;
+			cmp = ac;
+		}
+		var lyrs = cmp.selectedLayers;
+		if(lyrs.length!=1){
+            alert("レイヤを1個だけ選んで")
+        }else{
+            ret = lyrs[0];
         }
 		return ret;
 	}
@@ -475,7 +541,7 @@ if ( typeof (FsJSON) !== "object"){//デバッグ時はコメントアウトす�
 
  	//-------------------------------------------------------------------------
  	//-------------------------------------------------------------------------
-	var winObj = ( me instanceof Panel) ? me : new Window("palette", "AE_RemapExceed", [ 0,  0,  250,  280]  ,{resizeable:true, maximizeButton:true, minimizeButton:true});
+	var winObj = ( me instanceof Panel) ? me : new Window("palette", "AE_RemapExceed", [ 0,  0,  250,  330]  ,{resizeable:true, maximizeButton:true, minimizeButton:true});
 	//-------------------------------------------------------------------------
 	var px = 10;
     var py = 10;
@@ -497,8 +563,13 @@ if ( typeof (FsJSON) !== "object"){//デバッグ時はコメントアウトす�
     py+=30;
 	var btnCenter = winObj.add("button", [px,py,px+btnW, py+btnH], "Center");
     py+=30;
-	var cbIsNoUseBD = winObj.add("checkbox", [px,py,px+btnW, py+btnH], "NO!ブロックディゾルブ");
-    cbIsNoUseBD.value = isNoUseBD;
+	var btnGetCellLayer = winObj.add("button", [px,py,px+btnW, py+btnH], "getCellLayer");
+    py+=30;
+	var cmbKoma = winObj.add("dropdownlist", [px,py,px+btnW, py+btnH], ["ディゾルブ","リマップ最大値","不透明度"]);
+    cmbKoma.items[emptyCell].selected = true;
+    py+=30;
+	var cbInOutPoint = winObj.add("checkbox", [px,py,px+btnW, py+btnH], "InOutPoint");
+    cbInOutPoint.value = inOutPoint;
     py+=60;
     px = 110;
     py = 40;
@@ -514,11 +585,19 @@ if ( typeof (FsJSON) !== "object"){//デバッグ時はコメントアウトす�
     btnFromComp.onClick = compToObj;
     btnSaveArdj.onClick = export_ardj;
 
-    cbIsNoUseBD.onClick = function()
-    {
-        isNoUseBD = cbIsNoUseBD.value;
-    }
     btnCenter.onClick = cenertAE_Reamp;
+    cmbKoma.onChange = function()
+    {
+        emptyCell = cmbKoma.selection.index;
+        savePref();
+
+    }
+    cbInOutPoint.onClick=function()
+    {
+        inOutPoint = cbInOutPoint.value;
+        savePref();
+    }
+
 	//-------------------------------------------------------------------------
     var clearRbtns = function()
     {
@@ -610,30 +689,75 @@ if ( typeof (FsJSON) !== "object"){//デバッグ時はコメントアウトす�
                 lyr.startTime = 0;
                 lyr.inPoint = 0;
                 lyr.outPoint = lyr.containingComp.duration;
+                var fr = lyr.containingComp.frameRate;
                 rp.setValuesAtTimes(times,values);
 		        for (var i=1 ; i<=rp.numKeys ; i++) {
                     rp.setInterpolationTypeAtKey(i,KeyframeInterpolationType.HOLD,KeyframeInterpolationType.HOLD);
                 }
-                if (isNoUseBD == false) {
-                    var eg = lyr.property("ADBE Effect Parade");
-                    var mn = "ADBE Block Dissolve";
-                    var na = "EmptyCell";
-                    if (eg.canAddProperty(mn)==true)
-                    {
-                        var bp = findProp(eg,mn,na);
-                        if (bp==null){
-                            bp = eg.addProperty(mn);
-                            bp.name = na;
+                switch(emptyCell)
+                {
+                    case 0:
+                        var eg = lyr.property("ADBE Effect Parade");
+                        var mn = "ADBE Block Dissolve";
+                        var na = "EmptyCell";
+                        if (eg.canAddProperty(mn)==true)
+                        {
+                            var bp = findProp(eg,mn,na);
+                            if (bp==null){
+                                bp = eg.addProperty(mn);
+                                bp.name = na;
+                            }
+                            var bpv = bp.property(1);
+                            if (bpv.numKeys>0) for ( var i=bpv.numKeys; i>=1;i--) bpv.removeKey(i);
+                            bpv.setValuesAtTimes(emptyTimes,emptys);
+                            for (var i=1 ; i<=bpv.numKeys ; i++) {
+                                bpv.setInterpolationTypeAtKey(i,KeyframeInterpolationType.HOLD,KeyframeInterpolationType.HOLD);
+                            }
                         }
-                        var bpv = bp.property(1);
-                        if (bpv.numKeys>0) for ( var i=bpv.numKeys; i>=1;i--) bpv.removeKey(i);
-                        bpv.setValuesAtTimes(emptyTimes,emptys);
-                        for (var i=1 ; i<=bpv.numKeys ; i++) {
-                            bpv.setInterpolationTypeAtKey(i,KeyframeInterpolationType.HOLD,KeyframeInterpolationType.HOLD);
-                        }
-                    }
+                        break;
+                    case 2:
+                        var opa = lyr.transform.opacity;
+                            if (opa.numKeys>0) for ( var i=opa.numKeys; i>=1;i--) opa.removeKey(i);
+                            for (var i=0; i<emptys.length;i++)
+                            {
+                                if(emptys[i]==100)
+                                {
+                                    emptys[i] =0;
+                                }else{
+                                    emptys[i] =100;
+                                }
+
+                            }
+                            opa.setValuesAtTimes(emptyTimes,emptys);
+                            for (var i=1 ; i<=opa.numKeys ; i++) {
+                                opa.setInterpolationTypeAtKey(i,KeyframeInterpolationType.HOLD,KeyframeInterpolationType.HOLD);
+                            }
+                        break;
+                        case 1:
+                    default:
+                        break;
                 }
+                if(inOutPoint==true)
+                {
+                    if(rp.numKeys>2)
+                    {
+                        var maxV = Math.round(lyr.source.duration*fr);
+                        var ff =  Math.round(rp.keyValue(1)*fr);
+                        if(ff>=maxV)
+                        {
+                            lyr.inPoint = rp.keyTime(2);
+                        }
+                        ff =  Math.round(rp.keyValue(rp.numKeys)*fr);
+                        if(ff>=maxV)
+                        {
+                            lyr.outPoint = rp.keyTime(rp.numKeys);
+                        }
+
+                    }
+                }else{
                 lyr.outPoint = lyr.containingComp.duration;
+                }
+
 
 
             }catch(e){
@@ -693,13 +817,205 @@ if ( typeof (FsJSON) !== "object"){//デバッグ時はコメントアウトす�
             }
 
             applySub(lyrs[i],times,values,emptys,emptyTimes);
-            lyrs[i].inPoint = 0;
-            lyrs[i].outPoint = duration;
+            //lyrs[i].inPoint = 0;
+            //lyrs[i].outPoint = duration;
         }
         app.endUndoGroup();
     }
     btnApply.onClick = applyCells;
 
+	//-------------------------------------------------------------------------
+    var getCellLayer = function()
+    {
+        // ********************************
+        function arrayFromMinus(ary)
+        {
+            var ret = [];
+            for (var i=0 ; i<ary.length;i++) ret.push(ary[i]);
+            for (var i=1 ; i<ret.length;i++)
+            {
+                if (ret[i]<0)
+                {
+                    ret[i] = ret[i-1];
+                }
+            }
+            return ret;
+
+        }
+        // ********************************
+        function arrayToMinus(ary)
+        {
+            var ret = [];
+            for (var i=0 ; i<ary.length;i++) ret.push(ary[i]);
+            for (var i=ret.length-1 ; i>=1;i--)
+            {
+                if (ret[i]==ret[i-1])
+                {
+                    ret[i] = -1;
+                }
+            }
+            return ret;
+
+        }
+        // ********************************
+        function findBD(lyr)
+        {
+            var ret = null;
+            var eg = lyr.property("ADBE Effect Parade");
+            if (eg.numProperties<=0) return ret;
+
+            for ( var i=1; i<=eg.numProperties;i++)
+            {
+                if (eg.property(i).matchName=="ADBE Block Dissolve")
+                {
+                    if (eg.property(i).active==true)
+                    {
+                        ret = eg.property(i)(1);
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+        // ********************************
+		var aeremapCall = new File(aeremapCallPath);
+		var cmd =  "\"" + aeremapCall.fsName +"\"";
+		if (aeremapCall.exists==false){
+            alert("ae_reampcallがありません");
+            return;
+        }
+        var r = system.callSystem(cmd + " /exenow");
+        r = r.trim().toLowerCase();
+        if (r=="false") {
+            alert("no process");
+            return ret;
+        }
+
+        var lyr = getLayerOne();
+        if (lyr==null) {
+            return;
+        }
+        var remap = lyr(2);
+        if (remap.numKeys<=0){
+            alert("リマップが未入力です");
+            return;
+        }
+        //コマデータを獲得
+        var obj = {};
+        obj.header = "ardjV2";
+        var du = lyr.containingComp.duration;
+        var fr = lyr.containingComp.frameRate;
+        var maxV = remap.maxValue;
+        obj.frameCount = Math.round(du*fr);
+        var fc = obj.frameCount;
+
+        var cellData = [];
+        for (var i =0; i<fc; i++) cellData.push(-1);
+        cellData[0] = 0;
+
+        var isFootage = (lyr.source instanceof FootageItem);
+
+        //リマップデータの獲得
+        for (var i=1; i<=remap.numKeys;i++)
+        {
+            var frm = Math.round(remap.keyTime(i)*fr);
+            var v = remap.keyValue(i);
+            if (v>=maxV){
+                if(isFootage==true){
+                    cellData[frm] = maxV*fr;
+                }else{
+                    cellData[frm]= 0;
+                }
+
+            }else{
+                cellData[frm] = Math.round(v * fr)+1;
+            }
+        }
+        cellData = arrayFromMinus(cellData);
+        //不透明度の獲得
+        var opa = lyr(6)(11);
+        if (opa.numKeys>=2)
+        {
+            var opaData = [];
+            for (var i =0; i<fc; i++) opaData.push(-1);
+            opaData[0] = 100;
+            for (var i=1; i<=opa.numKeys;i++)
+            {
+                var frm = Math.round(opa.keyTime(i)*fr);
+                opaData[frm] = opa.keyValue(i);
+            }
+            opaData = arrayFromMinus(opaData);
+            for (var i=0; i<fc;i++)
+            {
+                if (opaData[i]<=0){
+                    cellData[i]=0;
+                }
+            }
+        }
+        var bd = findBD(lyr);
+        if (bd!=null)
+        {
+            if (bd.numKeys>=2)
+            {
+                var bddata = [];
+                for (var i =0; i<fc; i++) bdData.push(-1);
+                bdData[0] = 0;
+                for (var i=1; i<=bd.numKeys;i++)
+                {
+                    var frm = Math.round(bd.keyTime(i)*fr);
+                    var v = bd.keyValue(i);
+                    bdData[frm] = v;
+                }
+                bdData = arrayFromMinus(bdData);
+                for (var i=0; i<fc;i++)
+                {
+                    if (bdData[i]==100){
+                        cellData[i]=0;
+                    }
+                }
+            }
+        }
+        //ardjに変換
+        cellData = arrayToMinus(cellData);
+        var data=[];
+        for (var i =0; i<fc; i++)
+        {
+            if (cellData[i]>=0)
+            {
+                var d = [];
+                d.push(i);
+                d.push(cellData[i]);
+                data.push(d);
+            }
+        }
+        obj.cell = data;
+
+        //データを保存
+        var json = FsJSON.toJSON(obj);
+
+       //return;
+
+
+        var saveFile = new File(Folder.temp.fullName + "/ae_remap_layer.json");
+        if (saveFile.open("w")==true)
+        {
+            try{
+                saveFile.write(json);
+            }catch(e){
+                alert("import_layer write error\r\n" + e.toString());
+            }finally{
+                saveFile.close();
+            }
+        }
+
+        try{
+            var r = system.callSystem(cmd + " /import_layer \"" + saveFile.fsName+"\"");
+
+        }catch(e){
+            alert("import_layer call err\r\n" + e.toString());
+        }
+    }
+    btnGetCellLayer.onClick = getCellLayer;
 	//-------------------------------------------------------------------------
     var resizeLayout = function()
     {
